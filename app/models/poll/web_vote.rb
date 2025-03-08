consuldemocracy/consuldemocracy

Answer all poll questions at the same time TODO
Name: because we already have Ballot, and because Vote might be mistaken with other vote classes

app/models/poll/web_vote.rb
@@ -1,0 +1,35 @@
+class Poll::WebVote
+  # TODO: handle errors, validations, ...
+  attr_reader :poll, :user
+
+  def initialize(poll, user)
+    @poll = poll
+    @user = user
+  end
+
+  def questions
+    poll.questions.for_render.sort_for_list
+  end
+
+  def update(params)
+    user.with_lock do
+      questions.each do |question|
+        question.answers.where(author: user).each(&:destroy!)
+        next unless params[question.id.to_s] # TODO: hidden field instead?
+
+        option_ids = params[question.id.to_s][:option_id]
+
+        answers = Array(option_ids).map do |option_id|
+          question.find_or_initialize_user_answer(user, option_id)
+        end
+
+        if answers.map(&:valid?).all?(true)
+          Poll::Voter.find_or_create_by!(user: user, poll: poll, origin: "web")
+          answers.each(&:save!)
+        else
+          raise ActiveRecord::Rollback
+        end
+      end
+    end
+  end
+end

AyuntamientoMadrid/participacion

app/models/poll/web_vote.rb
@@ -1,0 +1,35 @@
+class Poll::WebVote
+  # TODO: handle errors, validations, ...
+  attr_reader :poll, :user
+
+  def initialize(poll, user)
+    @poll = poll
+    @user = user
+  end
+
+  def questions
+    poll.questions.for_render.sort_for_list
+  end
+
+  def update(params)
+    user.with_lock do
+      questions.each do |question|
+        question.answers.where(author: user).find_each(&:destroy!)
+        next unless params[question.id.to_s] # TODO: hidden field instead?
+
+        option_ids = params[question.id.to_s][:option_id]
+
+        answers = Array(option_ids).map do |option_id|
+          question.find_or_initialize_user_answer(user, option_id)
+        end
+
+        if answers.map(&:valid?).all?(true)
+          Poll::Voter.find_or_create_by!(user: user, poll: poll, origin: "web")
+          answers.each(&:save!)
+        else
+          raise ActiveRecord::Rollback
+        end
+      end
+    end
+  end
+end